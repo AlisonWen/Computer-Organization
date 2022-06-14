162f7ffb7f351a5c8b03be7f0f7c9c4e5db0f14e
Alison Wen <77866896+AlisonWen@users.noreply.github.com>
2022-06-14 22:33:51 +0800

    Update direct_mapped_cache.cpp

--- Lab 6/direct_mapped_cache.cpp
+++ Lab 6/direct_mapped_cache.cpp
@@ -15,10 +15,11 @@ unordered_map <char, string> Hex_to_binary;
 unordered_map <string, string> cache;
 
 float direct_mapped(string filename, int block_size, int cache_size){
-    int total_num = -1;
+    int total_num = 0;
     int hit_num = 0;
     int index_len = log2(cache_size / block_size);
     int offset_len = log2(block_size);
+    
     ifstream input_stream(filename);
     string s;
     Hex_to_binary['0'] = "0000";
@@ -42,18 +43,17 @@ float direct_mapped(string filename, int block_size, int cache_size){
         int tmp[35];
         string addr = "";
         while (s.size() < 8) s = "0" + s;
+
+        for(int i = 0; i < s.size(); i++) addr = addr + Hex_to_binary[s[i]];
         
-        for(int i = 0; i < s.size(); i++){
-            addr = addr + Hex_to_binary[s[i]];
-        }
         string index = addr.substr(32 - index_len - offset_len, index_len);
-        
-        if(cache.find(index) != cache.end()) hit_num++;
-        else cache[index] = addr;
-        
+        string tag = addr.substr(0, 32 - index_len - offset_len);
+
+        if(cache.find(index) != cache.end() && cache[index] == tag){
+            hit_num++;
+        }else cache[index] = tag;
         total_num++;
     }
-    
      
   
     return (float)hit_num/total_num;
