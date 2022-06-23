fafc60c074e8cfb229be072601898b30d63c9cc5
Alison Wen <77866896+AlisonWen@users.noreply.github.com>
2022-06-23 10:07:34 +0800

    Create Adder.v
    A team project with @ZoLArk173

---
+++ Lab 5/Adder.v
@@ -0,0 +1,13 @@
+`timescale 1ns/1ps
+
+module Adder(
+    input  [32-1:0] src1_i,
+    input  [32-1:0] src2_i,
+    output reg [32-1:0] sum_o
+);
+
+always @(*) begin
+    sum_o <= src1_i + src2_i;
+end
+
+endmodule
